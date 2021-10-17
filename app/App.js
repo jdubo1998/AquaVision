34d3ea680860a36ecd374c1eab753f7c8b1c8d3c
jdubo1998 <jdubo1998@tamu.edu>
2021-10-17 11:55:45 -0500

    Edited Server
    Changed the server file to run in debug mode which allows for code changes to be pushed automatically.

--- app/App.js
+++ app/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { io } from 'socket.io-client';  
 
-const socket = io('http://localhost:5000')
+const socket = io('http://127.0.0.1:5000')
 
 export default function App() {
     console.log(socket)
 
     return (
-        <body>
+        <div>
             safaf
-        </body>
+        </div>
     )
 }
+
+socket.on('connect_error', (err) => {
+    console.log(err.message);
+ })
--- src/python/server.py
+++ src/python/server.py
@@ -1,19 +1,30 @@
 from flask import Flask
+from flask_cors import CORS
 import socketio
+import sys 
 
 class AquaVisionServer:
     def __init__(self):
         self.sio = socketio.Server(async_mode='threading')
         self.app = Flask(__name__)
-        self.app.wsgi_app = socketio.WSGIApp(self.sio, self.app.wsgi_app)
+        CORS(self.app, origins=['*'])
+        self.app.wsgi_app = socketio.WSGIApp(self.sio, wsgi_app=self.app.wsgi_app, static_files={'/':'test.html'})
     
-    def start(self):
-        self.app.run()
+    def start(self, h):
+        self.app.run(host=h, debug=True)
 
 server = AquaVisionServer()
 
 def main():
-    server.start()
+    host = '0.0.0.0'
+
+    if (len(sys.argv) > 1):
+        if (sys.argv[1] == '1'):
+            host = ''
+        else:
+            host = sys.argv[1]
+    
+    server.start(host)
 
 @server.sio.event
 def connect(sid, environ):
