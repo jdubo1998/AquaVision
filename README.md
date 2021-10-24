41116c6d1a6bd7e1c677ba0902b71cd3e76aea43
jdubo1998 <jdubo1998@tamu.edu>
2021-10-24 15:22:36 -0500

    Added GPS coordinates text to the react-native app using useStates. Added instructions for testing GPS to the README.

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ CSCE 483 Project 1
 **const socket = io('http://X.X.X.X:5000/')**
 
 7. npx expo-cli start
+
+## Testing GPS Module
+1. cd AquaVision\src\python
+2. python3 .\integration_testing.py (Tests both socket.io and GPS functionality.)
+3. Run the react-native web app.
+4. Press any button to update the GPS coordinates.
+\
+Notes:
+The flask-socektio sever can only emit messages on the same thread that the server was initialized on. Because of this, I have not yet implemented a polling thread for the GPS module. Thus, only when the server receives a command does it run the code to get the GPS data.
--- app/App.js
+++ app/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, LogBox, TouchableOpacity, TextInput } from 'react-native';
 // ^^^ All these are libraries used to create the html objects below.
 import { io } from 'socket.io-client';
 import styles from './style.js';
 import Icon from 'react-native-vector-icons/FontAwesome5' //this library has standard icons we used for the buttons
 
-const socket = io('http://10.3.141.1:5000/') // Change to IP address of the device hosting the server.
+const socket = io('http://192.168.0.24:5000/') // Change to IP address of the device hosting the server.
 
 export default function App() {
+    const [gpsCoordinates, setGpsCoordinates] = useState("")
+
+    /***   Socket Functions   ***/
+    /* Event that triggers when the command that is sent to the Translate Module. */
+    socket.on('relaycommand', (command) => {})
+
+    /* Event that triggers when the data received from the Translate Module. */
+    socket.on('relaydata', (data) => {
+        setGpsCoordinates(data)
+    })
+
+    /* Event that triggers when a successful connetion is established. */
+    socket.on('connect', () => {
+        console.log(`Connected with id: ${socket.id}`)
+    })
+
+    socket.on('connect_error', (err) => {
+        console.log(err.message);
+    })
+
+    socket.on("disconnect", (reason) => {
+        console.log(reason)
+        /* If it was the server that disconnected, manually reconnect. */
+        if (reason === "io server disconnect") {
+            socket.connect()
+        }
+        // If it was not the server that disconnected, then it automatically reconnect.
+    })
+
     return (
         //this div holds everything inside it
         <div> 
             {/* This VIEW object is a container for all the buttons. The reason its in a container is to style */}
             <View style={styles.gpsLabel}>
-            <TextInput style = {styles.gpsText}
+            <Text style = {styles.gpsText}
+            autoCapitalize = 'characters'
             //    underlineColorAndroid = "transparent"
-               placeholder = "GPS Coordinates"
             //    placeholderTextColor = "#9a73ef"
-               autoCapitalize = 'characters'
-               />
+            >
+                {gpsCoordinates}
+            </Text>
+            
+               
             </View>
             <View style={styles.container}>
                 {/* This view is a container for button 1 and is repeated below...*/}
@@ -58,49 +90,22 @@ export default function App() {
                 </View>
             </View>
         </div>
-    );
-}
+    )
 
-/***   Button Functions   ***/
-function screenshotFunction() {
-    socket.emit('relaycommand', 'screenshot');
-}
-
-function lightFunction() {
-    socket.emit('relaycommand', 'lights');
-}
-
-function moveUpFunction() {
-    socket.emit('relaycommand', 'moveup');
-}
-
-function moveDownFunction() {
-    socket.emit('relaycommand', 'movedown');
-}
-
-/***   Socket Functions   ***/
-/* Event that triggers when the command that is sent to the Translate Module. */
-socket.on('relaycommand', (command) => {})
-
-/* Event that triggers when the data received from the Translate Module. */
-socket.on('relaydata', (data) => {
-    console.log(`Received data: ${data}`) // GPS Data.
-})
-
-/* Event that triggers when a successful connetion is established. */
-socket.on('connect', () => {
-    console.log(`Connected with id: ${socket.id}`)
-})
-
-socket.on('connect_error', (err) => {
-    console.log(err.message);
-})
-
-socket.on("disconnect", (reason) => {
-    console.log(reason)
-    /* If it was the server that disconnected, manually reconnect. */
-    if (reason === "io server disconnect") {
-        socket.connect()
+    /***   Button Functions   ***/
+    function screenshotFunction() {
+        socket.emit('relaycommand', 'screenshot');
     }
-    // If it was not the server that disconnected, then it automatically reconnect.
-})
+
+    function lightFunction() {
+        socket.emit('relaycommand', 'lights');
+    }
+
+    function moveUpFunction() {
+        socket.emit('relaycommand', 'moveup');
+    }
+
+    function moveDownFunction() {
+        socket.emit('relaycommand', 'movedown');
+    }
+}
