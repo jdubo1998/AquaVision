b2337ccf4202d9d76108e316892c520815dcdbd4
Jacob DuBois <jdubo1998@tamu.edu>
2021-11-05 05:12:20 +0000

    Added comments to the LoRa radio module.

--- src/python/lora_radio.py
+++ src/python/lora_radio.py
@@ -3,14 +3,25 @@ from serial.serialutil import SerialException
 from threading import Thread
 
 class LoRaRadio():
-    def __init__(self, port='/dev/serial0', baudrate=9600, timeout=0.5, target=None):
+    def __init__(self, addr, port='/dev/serial0', baudrate=9600, timeout=0.5):
         self.ser = Serial(port, baudrate, timeout=timeout, parity=PARITY_NONE, stopbits=1)
+        self.addr = addr
         self.recv_thread = Thread(target=self._recv_thread)
         self.open()
 
+    # Writes to the serial port.
     def write_serial(self, command):
-        self.ser.write(str.encode('{}\r\n'.format(command), encoding='utf-8'))   
+        self.ser.write(str.encode('{}\r\n'.format(command), encoding='utf-8'))
 
+    # Quick method to send a message to another LoRa module.
+    # FFFF is the broadcast address.
+    def send_message(self, message, addr='FFFF'):
+        # Converts message to an 8 byte hex array.
+        message_hex = ''.join('{:02x}'.format(ord(c)) for c in i.ljust(10))
+
+        self.write_serial('AT+SEND {},"{}"'.format(addr, message_hex))
+
+    # Reads from the serial port.
     def read_serial(self):
         try:
             response = self.ser.readline()
@@ -19,12 +30,7 @@ class LoRaRadio():
         except SerialException as e:
             print(e)
 
-    def send_message(self, message, addr='FFFF'):
-        # Converts message to an 8 byte hex array.
-        message_hex = ''.join('{:02x}'.format(ord(c)) for c in i.ljust(10))
-
-        self.write_serial('AT+SEND {},"{}"'.format(addr, message_hex))
-
+    # Interprets a received message and runs the appropriate function based on the contents.
     def interpret_response(self, response):
         s = str(response, 'utf-8', errors='ignore')
 
@@ -39,18 +45,13 @@ class LoRaRadio():
         elif s.__contains__('gps'):
             print('Get GPS')
 
-    def _recv_thread(self):
-        while self.receiving:
-            self.ser.write('AT+RECV\r\n'.encode('utf-8'))
-            self.read_serial()
-        
-        print('Closed recv thread.')
-
+    # Toggles between two modes: Receiving and Transmitting mode.
     def toggle_mode(self):
         if self.receiving:
             self.receiving = False
             
             self.write_serial('+++')
+            self.write_serial('AT+SADDR {}'.format(self.addr))
             self.write_serial('AT+ROLE 1')
             self.write_serial('AT+USERMODE 0')
 
@@ -59,6 +60,7 @@ class LoRaRadio():
             self.receiving = True
             self.recv_thread.start()
 
+    # Opens the serial port and sets the LoRa parameters to the default configuration.
     def open(self):
         self.receiving = True
         self.toggle_mode()
@@ -66,12 +68,21 @@ class LoRaRadio():
         if not self.ser.is_open:
             self.ser.open()
 
+    # Closes the serial port and any thread that is polling for received data.
     def close(self):
         self.receiving = False
         self.ser.close()
 
+    # Polls the serial port to read any messages received by the LoRa radio.
+    def _recv_thread(self):
+        while self.receiving:
+            self.ser.write('AT+RECV\r\n'.encode('utf-8'))
+            self.read_serial()
+
 if __name__ == '__main__':
-    radio = LoRaRadio()
+    # Creates a radio module with the address 0001.
+    # The address is a 2-byte hexidecmial which uses this conversion: ABCD => CDAB
+    radio = LoRaRadio('0100')
 
     try:
         while True:
